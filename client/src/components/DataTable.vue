18213aae8bd5ef1f796a0cd492c66d3eb9913b2e
RudyLang <r.lang.dev@gmail.com>
2020-09-29 20:11:43 -0400

    Added proper table headings an variables.

--- client/src/components/DataTable.vue
+++ client/src/components/DataTable.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title>
-      Reports
+      Unity Builds - 2020.1.61f
       <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
@@ -15,10 +15,19 @@
         :headers="headers"
         :items="reports"
         :search="search"
-        :items-per-page="5">
+        :items-per-page="10">
 
-        <template v-slot:item.status="{ item }">
-            <v-chip :color="getColour(item.status)" dark>{{ item.status }}</v-chip>
+        <template v-slot:item.build_status="{ item }">
+            <v-chip 
+              :color="getColour(item.build_status)" 
+              dark
+              label>
+              {{ item.build_status }}
+            </v-chip>
+        </template>
+
+        <template v-slot:item.build_link="{ item }">
+          <a v-bind:href="''">{{ item.build_link }}</a>
         </template>
     </v-data-table>
   </v-card>
@@ -30,22 +39,23 @@ export default {
     return {
       search: "",
       headers: [
-        {
-          text: "Type",
-          align: "start",
-          sortable: true,
-          value: "type",
-        },
-        { text: "Comment", value: "comment" },
-        { text: "Timestamp", value: "timestamp" },
-        { text: "Status", value: "status" },
+        { text: "Number", align: "start", sortable: true, value: "build_number" },
+        { text: "Type", sortable: true, value: "build_type" },
+        { text: "Size", sortable: true, value: "build_size" },
+        { text: "Time", sortable: true, value: "build_time" },
+        { text: "Status", sortable: true, value: "build_status" },
+        { text: "Timestamp", sortable: true, value: "timestamp" },
+        { text: "Link", sortable: true, value: "build_link" }
       ],
       reports: [
         {
-          type: "Test",
-          comment: "Comment",
-          timestamp: "Thursday",
-          status: 'FAIL',
+          build_number: "37",
+          build_type: "Release",
+          build_size: "56.6 mb",
+          build_time: "00:00:06",
+          build_status: "Successful",
+          timestamp: "2020-09-29 19:53:22",
+          build_link: "http://localhost:8081/job/TestGame/43/"
         },
       ],
     };
@@ -53,9 +63,15 @@ export default {
 
   methods: {
       getColour (status) {
-          if ( status === 'FAIL' ) return 'red';
+          if ( status === 'Failed' ) return 'red';
           else return 'green';
       }
   }
 };
 </script>
+
+<style>
+td {
+  color: rgb(255, 222, 77);
+}
+</style>
